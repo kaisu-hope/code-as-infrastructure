2064e30d8cb36006d6f71b97e978db3a51af0598
kaisu <ks2997854254@gmail.com>
2022-09-29 10:31:56 +0800

    feat: kubernetes 1.25.2-00

--- vagrant-vm-env/k8s/master.sh
+++ vagrant-vm-env/k8s/master.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-MASTER_IP=192.168.31.88
-NETWORK_SEGMENT=192.168.31
+MASTER_IP=192.168.1.88
+NETWORK_SEGMENT=192.1.1
 # 设置ip
 sudo echo "Environment=\"KUBELET_EXTRA_ARGS=--node-ip=`ip a|grep -w 'inet'|grep 'global'|sed 's/^.*inet //g'|sed 's/\/[0-9][0-9].*$//g' | grep $NETWORK_SEGMENT`\"" | sudo tee -a /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 sudo systemctl daemon-reload
@@ -27,15 +27,25 @@ sudo chmod +x /vagrant/configs/join.sh
 sudo kubeadm token create --print-join-command > /vagrant/configs/join.sh
 # 安装calico
 kubectl apply -f /vagrant/calico.yaml
-
-#sudo apt install nfs-kernel-server -y
-#
-#sudo mkdir -p /data/nfs/
-#
-#sudo chmod 777 /data/nfs/
-#
-#sudo sed -i "$ a /data/nfs/ *(rw,sync,no_subtree_check,no_root_squash)" /etc/exports
-#
-#sudo exportfs -ra
-#
-#sudo systemctl restart nfs-kernel-server
+# 安装nfs
+apt install nfs-kernel-server -y
+mkdir -p /data/nfs/
+chmod 777 /data/nfs/
+sed -i "$ a /data/nfs/ *(rw,sync,no_subtree_check,no_root_squash)" /etc/exports
+exportfs -ra
+systemctl restart nfs-kernel-server
+# 安装storageClass
+kubectl apply -f /vagrant/nfs
+# 安装istio
+tar zxvf /vagrant/istio/istio-1.15.1-linux-amd64.tar.gz -C /opt
+mv /opt/istio-* /opt/istio
+tar zxvf /vagrant/istio/istioctl-1.15.1-linux-amd64.tar.gz -C /usr/local/bin/
+chmod +x /usr/local/bin/istioctl
+export PATH=/opt/istio/bin:$PATH
+ctr -n=k8s.io i pull docker.io/istio/proxyv2:1.15.1
+ctr -n=k8s.io i pull docker.io/istio/pilot:1.15.1
+istioctl install --set profile=default -y
+# 安装helm
+tar -zxvf /vagrant/helm-v3.10.0-linux-amd64.tar.gz
+mv /vagrant/linux-amd64/helm /usr/local/bin/helm
+rm -rf /vagrant/linux-amd64
--- vagrant-vm-env/k8s/node.sh
+++ vagrant-vm-env/k8s/node.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 # 设置ip
-NETWORK_SEGMENT=192.168.31
-sudo echo "Environment=\"KUBELET_EXTRA_ARGS=--node-ip=`ip a|grep -w 'inet'|grep 'global'|sed 's/^.*inet //g'|sed 's/\/[0-9][0-9].*$//g' | grep $NETWORK_SEGMENT`\"" | sudo tee -a /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-sudo systemctl daemon-reload
-sudo systemctl restart kubelet
+NETWORK_SEGMENT=192.168.1
+MASTER_IP=192.168.2.88
+echo "Environment=\"KUBELET_EXTRA_ARGS=--node-ip=`ip a|grep -w 'inet'|grep 'global'|sed 's/^.*inet //g'|sed 's/\/[0-9][0-9].*$//g' | grep $NETWORK_SEGMENT`\"" | sudo tee -a /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
+systemctl restart kubelet
 # 加入集群
-sudo /vagrant/configs/join.sh -v
+/vagrant/configs/join.sh -v
+# nfs配置
+apt-get install nfs-common -y
+mkdir -p /data/nfs/
+mount -t nfs -o nolock -o tcp $MASTER_IP:/data/nfs/ /data/nfs/
+ctr -n=k8s.io i pull docker.io/istio/proxyv2:1.15.1
+ctr -n=k8s.io i pull docker.io/istio/pilot:1.15.1
